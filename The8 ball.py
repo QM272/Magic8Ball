686c1b7f4ce7a9fa2a9d5eac82b3843e28de21a9
QM272 <134895744+QM272@users.noreply.github.com>
2023-08-21 19:55:47 -0400

    Fixing Program
    Made restarting options more user friendly, fixed wording and grammatical errors. Fixed functions.

--- The8 ball.py
+++ The8 ball.py
@@ -1,33 +1,42 @@
-#8 ball or something like that
+#8 ball program
 #200802
 
 import random
 import time
+
+#All possible 8-Ball answers alongside the decision to play agian
 options = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definately.", "You may rely on it", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Dont count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
+yesorno = ["Y","N"]
 
-
-print ("Behold. THE 8 Ball. (THE8 hee hee)")
+print("")
+print ("Behold. The 8 Ball.")
 print ("")
-print ("")
 
-def themainstuff():
-     print ("Ask away, amigo.")
+def question():
+     print("")
+     print ("Ask a question to reveal your future.")
      input()
      print ("")
      print ("Processing...")
      time.sleep (3)
      print ("")
      print (random.choice(options))
-     print ("")
-     print ("There you go.")
      askagain()
      
 def askagain():
      print ("")
-     print ("Do you have another question? If so... then")
-     themainstuff()
+     playagain = ""
+     playagain = input ("Do you have another question? If yes, press Y. If no , press N.")
+     if playagain == "Y" or playagain == "y":
+          question()
+     elif playagain == "N" or playagain == "n":
+          print ("")
+          print ("Thank you for playing")
+          exit()
+     else: 
+          print ("")
+          print ("I did not understand. Please repeat.")
 
 
-
-themainstuff()
+question()
 askagain()
